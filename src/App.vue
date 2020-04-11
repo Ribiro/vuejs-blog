5ff40bac7983fa244b2a4d0bcd382635b44e07a6
Ribiro <ribiro05@gmail.com>
2020-04-12 00:24:41 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <app-header></app-header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import addBlog from "./components/addBlog";
+import showBlogs from "./components/showBlogs";
+import header from "./components/header";
+
+export default {
+  components: {
+      'add-blog': addBlog,
+      'show-blog': showBlogs,
+      'app-header': header
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style>
+  body{
+    margin: 0;
+    font-family: "Open Sans Semibold";
+  }
+</style>
